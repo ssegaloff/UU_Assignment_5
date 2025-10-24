ab8856c4704731d0bc4cbd3f0aadc0204271d979
ssegaloff <ssegaloff18@gmail.com>
2025-10-23 21:02:36 -0400

    updates

--- q3.py
+++ q3.py
@@ -5,7 +5,8 @@ import pandas as pd
 
 
 # %%
-# Load the `cville_weather.csv` data. This includes data from Jan 4, 2024 to Feb 2, 2025. Are there any missing data issues?
+# Load the `cville_weather.csv` data. This includes data from Jan 4, 2024 to Feb 2, 2025. 
+# Are there any missing data issues?
 weather = pd.read_csv('cville_weather.csv')
 weather
 
@@ -14,8 +15,10 @@ weather
 
 
 # %%
-# Based on the precipitation variable, `PRCP`, make a new variable called `rain` that takes the value 1 if `PRCP`>0 and 0 otherwise.
-
+# Based on the precipitation variable, `PRCP`, make a new variable called `rain` 
+# that takes the value 1 if `PRCP`>0 and 0 otherwise.
+weather['rain'] = [1 if x > 0 else 0 for x in weather['PRCP']]
+weather
 
 
 
@@ -26,13 +29,15 @@ weather
 
 
 # %%
-# For your chain from c, how likely is it to rain if it was rainy yesterday? How likely is it to rain if it was clear yesterday?
+# For your chain from c, how likely is it to rain if it was rainy yesterday? 
+# How likely is it to rain if it was clear yesterday?
 
 
 
 
 # %% 
-# Starting from a clear day, forecast the distribution. How quickly does it converge to a fixed result? What if you start from a rainy day?
+# Starting from a clear day, forecast the distribution. 
+# How quickly does it converge to a fixed result? What if you start from a rainy day?
 
 
 
